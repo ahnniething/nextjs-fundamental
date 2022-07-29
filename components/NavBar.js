a18d9c34b9e7e8f81f31f914bb73251bab374667
ahnniething <ahnniething@gmail.com>
2022-07-29 16:10:03 +0900

    style: add multiple styles using the way of <style jsx>

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,15 +4,63 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   const router = useRouter();
   return (
+    // the way of adding styles to component in NextJS
+    <>
+    {/* 1. CSS module */}
     <nav>
       <Link href="/">
-        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : ""
+          ].join(" ")}
+        >
           About
         </a>
       </Link>
+
     </nav>
+
+   {/* 2. Styles JSX */}
+    <nav>
+    <Link href="/">
+        <a
+            className={router.pathname === "/" ? "active" : ""}
+        >
+          Home
+        </a>
+      </Link>
+      <Link href="/about">
+        <a
+   className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </a>
+      </Link>
+      <style jsx>{`
+        a {
+            margin: 10px;
+           text-decoration: none;
+        }
+        nav {
+            background-color: tomato;
+        }
+        .active{
+            color:dodgerblue;
+        }
+      `}</style>
+
+    </nav>
+
+    </>
   );
 }
