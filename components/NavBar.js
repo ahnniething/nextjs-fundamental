6aeb704078610271a985729191d41acfc71d763e
ahnniething <ahnniething@gmail.com>
2022-07-29 12:13:25 +0900

    feat: update navigation style using the <a> tag inside the <Link> tag

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 export default function NavBar(){
     return <nav>
-        <Link href="/">
-        <a>Home</a>
-        </Link>
-        <Link href="/about">
-        <a>About</a>
-        </Link>
+          <Link href="/">
+        <a style={{color:"red", margin: 10}}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: "blue",  margin: 10 }}>
+          About
+        </a>
+      </Link>
     </nav>
 }
