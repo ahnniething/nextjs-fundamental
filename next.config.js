f8536c520b47481a262d9d41dea0b0f537a9703a
ahnniething <ahnniething@gmail.com>
2022-07-31 22:45:23 +0900

    feat: add redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+      {
+        source: "/google",
+        destination: "https://www.google.com/",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path",
+        destination: "/new-blog/:path",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
